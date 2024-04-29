7113b944240d61f848a5f965130242f7c8da9f53
Ricardo Espinoza <respinoza@mgcertifica.cl>
2024-04-28 22:09:50 -0400

    Forms Validators

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -13,15 +13,18 @@ export class BasicPageComponent {
   //   inStorage: new FormControl(0)
   // })
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    name: ['', [ Validators.required, Validators.minLength(3) ] ],
+    price: [0, [ Validators.required, Validators.min(0) ] ],
+    inStorage: [0, [ Validators.required, Validators.min(0) ] ],
   })
 
   constructor( private fb: FormBuilder ) {}
 
 
   onSave():void {
+
+    if ( this.myForm.invalid ) return;
+
     console.log(this.myForm.value);
   }
 
