c8a8d82d93f93505b609650053c3599d261074ac
Ricardo Espinoza <respinoza@mgcertifica.cl>
2024-04-28 22:43:02 -0400

    Help methods for errors

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -27,12 +27,38 @@ export class BasicPageComponent implements OnInit {
   constructor( private fb: FormBuilder ) {}
 
   ngOnInit(): void {
-    this.myForm.reset( rtx5090 )
+    // this.myForm.reset( rtx5090 )
+  }
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+          return 'This camp is required';
+
+          case 'minlength':
+            return `Minimum ${ errors['minlength'].requiredLength } characters`;
+      }
+    }
+
+    return null;
   }
 
   onSave():void {
 
-    if ( this.myForm.invalid ) return;
+    if ( this.myForm.invalid )
+      this.myForm.markAllAsTouched();
+      return;
 
     console.log(this.myForm.value);
 
