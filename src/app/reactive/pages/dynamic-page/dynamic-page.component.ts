38a702f1820d3913aa2c49b33f5e783d5246f2bb
Ricardo Espinoza <respinoza@mgcertifica.cl>
2024-05-07 10:22:24 -0400

    Forms Validators

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -21,6 +21,41 @@ export class DynamicPageComponent {
     return this.myForm.get('favoriteGames') as FormArray;
   }
 
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  isValidFieldInArray( formArray: FormArray, index: number) {
+    return formArray.controls[index].errors
+        && formArray.controls[index].touched;
+  }
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+          return 'This camp is required';
+
+          case 'minlength':
+            return `Minimum ${ errors['minlength'].requiredLength } characters`;
+      }
+    }
+
+    return null;
+  }
+
+
+
+  onDeleteFavorite( index: number ): void {
+    this.favoriteGames.removeAt(index);
+  }
+
   onSubmit():void {
 
     if (this.myForm.invalid ) {
